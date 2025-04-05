cf6dcffbdaa0984696f9801344711ca26fea1c6e
Alejandro Jorgen Marten <ajorgenmarten35@gmail.com>
2025-04-05 02:24:23 -0400

    🔨 refactor: Cambios de estilo visual en el componente navbar

--- src/components/ui/Navbar/PlayersStatus.tsx
+++ src/components/ui/Navbar/PlayersStatus.tsx
@@ -4,7 +4,7 @@ import { Users } from "../../icons/users";
 export function PlayersStatus() {
   const { playersOnline } = useGame();
   return (
-    <div className="badge badge-outline border-violet-700 py-4 px-4 border-2 select-none bg-gray-950/30">
+    <div className="badge badge-outline border-primary/60 py-4 px-4 border-2 select-none bg-gray-950/30">
       <Users className="size-4 inline" /> {playersOnline} Online 🟢
     </div>
   );
--- src/components/ui/Navbar/index.tsx
+++ src/components/ui/Navbar/index.tsx
@@ -3,7 +3,7 @@ import { PlayersStatus } from "./PlayersStatus";
 
 export function Navbar() {
   return (
-    <div className="navbar bg-primary text-primary-content">
+    <div className="navbar border-b-[1px] border-gray-300/50 text-primary-content">
       <div className="flex items-center justify-between w-full">
         <h1 className="text-2xl font-bold flex items-center gap-1">
           <Gamepad className="size-8 text-violet-200 " />
